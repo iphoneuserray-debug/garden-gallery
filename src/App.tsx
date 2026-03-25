ac83378d3bc2d211ae3dd1f4adf6e4bfcf3a82cc
Shangren <zhoushangren@outlook.com>
2026-03-25 16:25:39 +1100

    added Login page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import Products from './pages/Products'
 import Detail from './pages/Detail'
 import Layout from './Layout'
+import Login from './components/Login'
 
 export default function App() {
     return (
@@ -12,6 +13,7 @@ export default function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="/products" element={<Products />} />
                     <Route path="/detail/:name" element={<Detail />} />
+                    <Route path="/login" element={<Login />} />
                 </Route>
             </Routes>
         </BrowserRouter>
